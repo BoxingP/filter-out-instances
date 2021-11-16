dc5410d418a248617617cef2955c80f124792967
Boxing <boxingp@gmail.com>
2021-11-16 15:59:02 +0800

    FO-3: add aws cloudformation outputs.

--- filter_out_instances/lambda_stack.py
+++ filter_out_instances/lambda_stack.py
@@ -72,3 +72,10 @@ class LambdaStack(cdk.Stack):
                                            timeout=cdk.Duration.seconds(900)
                                            )
         lambda_function.apply_removal_policy(cdk.RemovalPolicy.DESTROY)
+
+        cdk.CfnOutput(self, 'OutputLambdaName',
+                      export_name='-'.join([construct_id, 'lambda name'.replace(' ', '-')]),
+                      value=lambda_function.function_name)
+        cdk.CfnOutput(self, 'OutputLambdaARN',
+                      export_name='-'.join([construct_id, 'lambda arn'.replace(' ', '-')]),
+                      value=lambda_function.function_arn)
